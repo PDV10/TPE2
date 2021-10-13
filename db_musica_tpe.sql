8c009ef36ef77e7c4b53524751672b84fdf39912
Patricio De Vicente <patooodv@gmail.com>
2021-10-13 16:37:38 -0300

    se modifica la base de datos

--- db_musica_tpe.sql
+++ db_musica_tpe.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-10-2021 a las 20:19:48
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+-- Tiempo de generación: 13-10-2021 a las 21:37:04
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -57,6 +57,7 @@ CREATE TABLE `musica` (
   `artista` varchar(100) NOT NULL,
   `album` varchar(100) NOT NULL,
   `anio` date NOT NULL,
+  `imagen` varchar(500) NOT NULL,
   `id_genero_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -64,20 +65,20 @@ CREATE TABLE `musica` (
 -- Volcado de datos para la tabla `musica`
 --
 
-INSERT INTO `musica` (`id_musica`, `nombreCancion`, `artista`, `album`, `anio`, `id_genero_fk`) VALUES
-(10, 'Nunca quise', 'Intoxicados', 'OTRO DIA EN EL PLANETA TIERRA', '2008-02-13', 3),
-(11, 'Fresco', 'WOS', 'Carabana', '2019-10-04', 6),
-(12, 'Llenos de magia', 'La Vela Puerca', 'A contra luz', '2016-07-08', 3),
-(13, 'Viejo Karma!', 'Las Pastillas Del Abuelo', '', '2017-11-12', 3),
-(14, 'Demaciado Loco', 'Paulo Londra', 'Home Run', '2019-05-23', 2),
-(15, 'Loco', 'Tiago PZK', 'Loco', '2021-09-16', 2),
-(16, 'Hazmelo', 'Tiago PZK', 'Hazmelo', '2021-05-26', 2),
-(17, 'Como si no importara', 'Emilia & Duki', 'Como si no importara', '2021-07-13', 2),
-(18, 'ADEMAS DE MI REMIX', 'Rusherking, Tiago PZK, KHEA, LIT Killah, Duki, Maria Becerra', 'A demas de mi', '2021-03-04', 2),
-(19, 'Nat Geo', 'Falke 912, Bhavi Ft. LIT Killah', 'Nat Geo', '2021-07-15', 2),
-(20, 'No me conocen (REMIX)', 'BANDIDO, DUKI, REI, TIAGO PZK', 'No me conocen', '2021-06-16', 2),
-(21, 'Prende la Cámara', 'FMK, Tiago PZK', 'Prende la Cámara', '2021-07-01', 2),
-(22, 'Rápido Lento', 'Emilia, Tiago PZK', 'Rápido Lento', '2021-09-30', 2);
+INSERT INTO `musica` (`id_musica`, `nombreCancion`, `artista`, `album`, `anio`, `imagen`, `id_genero_fk`) VALUES
+(10, 'Nunca quise', 'Intoxicados', 'OTRO DIA EN EL PLANETA TIERRA', '2008-02-13', '', 3),
+(11, 'Fresco', 'WOS', 'Carabana', '2019-10-04', '', 6),
+(12, 'Llenos de magia', 'La Vela Puerca', 'A contra luz', '2016-07-08', '', 3),
+(13, 'Viejo Karma!', 'Las Pastillas Del Abuelo', '', '2017-11-12', '', 3),
+(14, 'Demaciado Loco', 'Paulo Londra', 'Home Run', '2019-05-23', '', 2),
+(15, 'Loco', 'Tiago PZK', 'Loco', '2021-09-16', '', 2),
+(16, 'Hazmelo', 'Tiago PZK', 'Hazmelo', '2021-05-26', '', 2),
+(17, 'Como si no importara', 'Emilia & Duki', 'Como si no importara', '2021-07-13', '', 2),
+(18, 'ADEMAS DE MI REMIX', 'Rusherking, Tiago PZK, KHEA, LIT Killah, Duki, Maria Becerra', 'A demas de mi', '2021-03-04', '', 2),
+(19, 'Nat Geo', 'Falke 912, Bhavi Ft. LIT Killah', 'Nat Geo', '2021-07-15', '', 2),
+(20, 'No me conocen (REMIX)', 'BANDIDO, DUKI, REI, TIAGO PZK', 'No me conocen', '2021-06-16', '', 2),
+(21, 'Prende la Cámara', 'FMK, Tiago PZK', 'Prende la Cámara', '2021-07-01', '', 2),
+(22, 'Rápido Lento', 'Emilia, Tiago PZK', 'Rápido Lento', '2021-09-30', '', 2);
 
 -- --------------------------------------------------------
 
@@ -136,7 +137,7 @@ ALTER TABLE `generos`
 -- AUTO_INCREMENT de la tabla `musica`
 --
 ALTER TABLE `musica`
-  MODIFY `id_musica` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_musica` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
