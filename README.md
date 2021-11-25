516d74cb06d467e3fbf19fdcf8e2286fc9f0a09b
Patricio De Vicente <patooodv@gmail.com>
2021-11-25 00:31:22 -0300

    se actualiza la base de datos final

--- README.md
+++ README.md
@@ -12,5 +12,9 @@ contraseña: admin
 usuario normal: user
 contraseña: user
 
+los ids de las canciones se optienen de un drive:
+(sabemos que no es la forma mas optima, pero es lo unico que se nos ocurrio)
+https://drive.google.com/drive/folders/1Wp_HyfqhTqULMxgOwXdhV1BptOMLUd34
+
 la carpeta pesa 80 mb debido a que el archivo ubicado en esta ruta pesa 77.000kb
 TPE2/.git/objects/pack
--- db_musica_tpe.sql
+++ db_musica_tpe.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2021 a las 01:08:51
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+-- Tiempo de generación: 25-11-2021 a las 04:30:55
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -84,7 +84,7 @@ CREATE TABLE `musica` (
   `anio` date NOT NULL,
   `favorito` tinyint(1) DEFAULT NULL,
   `imagen` varchar(500) DEFAULT NULL,
-  `cancion` varchar(200) NOT NULL,
+  `cancion` varchar(200) DEFAULT NULL,
   `id_genero_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -94,49 +94,49 @@ CREATE TABLE `musica` (
 
 INSERT INTO `musica` (`id_musica`, `nombreCancion`, `artista`, `album`, `anio`, `favorito`, `imagen`, `cancion`, `id_genero_fk`) VALUES
 (10, 'Nunca quise', 'Intoxicados', 'OTRO DIA EN EL PLANETA TIERRA', '2008-02-13', 0, 'img/NuncaQuise.jpg', '1c36WTqJNiOgXHTSqEckuLmpnmhrqbANM', 3),
-(11, 'Fresco', 'WOS', 'Carabana', '2019-10-04', 0, 'img/fresco.jpg', 'music/audio2.mp3', 6),
-(12, 'Llenos de magia', 'La Vela Puerca', 'A contra luz', '2016-07-08', 0, 'img/llenosDeMagia.jpg', 'music/llenosDeMagia.mp3', 3),
-(13, 'Viejo Karma!', 'Las Pastillas Del Abuelo', 'Desafios', '2017-11-12', 0, 'img/ViejoKarma.jpg', 'music/viejoKarma.mp3', 3),
-(14, 'Demaciado Loco', 'Paulo Londra', 'Home Run', '2019-05-23', 1, 'img/DemaciadoLoco.jpg', 'music/', 2),
-(15, 'Loco', 'Tiago PZK', 'Loco', '2021-09-16', 0, 'img/Loco.jpg', 'music/', 2),
-(16, 'Hazmelo', 'Tiago PZK', 'Hazmelo', '2021-05-26', 1, 'img/Hazmelo.jpg', 'music/', 2),
-(17, 'Como si no importara', 'Emilia & Duki', 'Como si no importara', '2021-07-13', 0, 'img/ComoSiNoImportara.jpg', 'music/', 2),
-(18, 'ADEMAS DE MI REMIX', 'Rusherking, Tiago PZK, KHEA, LIT Killah, Duki, Maria Becerra', 'A demas de mi', '2021-03-04', 1, 'img/AdemasDeMiRemix.jpg', 'music/', 2),
-(19, 'Nat Geo', 'Falke 912, Bhavi Ft. LIT Killah', 'Nat Geo', '2021-07-15', 0, 'img/NatGeo.jpg', 'music/', 2),
-(20, 'No me conocen (REMIX)', 'BANDIDO, DUKI, REI, TIAGO PZK', 'No me conocen', '2021-06-16', 1, 'img/NoMeConocen (REMIX).jpg', 'music/', 2),
-(21, 'Prende la Cámara', 'FMK, Tiago PZK', 'Prende la Cámara', '2021-07-01', 0, 'img/PrendeLaCámara.jpg', 'music/', 2),
-(22, 'Rápido Lento', 'Emilia, Tiago PZK', 'Rápido Lento', '2021-09-30', 0, 'img/RápidoLento.jpg', 'music/', 2),
-(26, 'M1 A1', 'Gorillaz', 'M1 A1', '2015-02-12', 0, 'img/gorillaz.jpg', 'music/m1a1.mp3', 3),
-(27, 'Ruta 66', 'Pappo\'s Blues', 'Caso Cerrado', '1995-03-15', 0, 'img/papo.jpg', 'music/ruta66.mp3', 3),
-(28, 'Balada Del Diablo y La Muerte', 'La Renga', 'Despedazado por mil partes', '1996-07-21', 0, 'img/LaRenga.jpg', 'music/baladaDelDiabloYLaMuerte.mp3', 3),
-(29, 'Toxicity ', 'System Of A Down', 'System Of A Down', '2001-09-04', 0, 'img/SystemOfaDown.jpg', 'music/toxicity.mp3', 3),
-(30, 'Crimen', 'Gustavo Cerati', 'Ahí Vamos', '2006-10-05', 0, 'img/cerati.jpg', 'music/', 3),
-(31, 'JIJIJI', 'Los Redondos', 'Oktubre', '1986-10-18', 1, 'img/indioSolari.jpg', 'music/', 3),
-(32, 'GALANG', 'Alika', 'EDUCATE YOURSELF', '2008-04-19', 0, 'img/alika.jpg', 'music/galang.mp3', 4),
-(33, 'Te robaste mi corazon', 'Fidel Nadal', 'Forever together', '2010-10-10', 0, 'img/fidelNadal.jpg', 'music/teRobasteMiCorazon.mp3', 4),
-(34, 'International love ', 'Fidel Nadal', 'INTERNATIONAL LOVE\"', '2008-10-16', 0, 'img/fidelNadal.jpg', 'music/internationalLove.mp3', 4),
-(35, 'Somewhere over the Rainbow', 'Israel \"IZ\" Kamakawiwoʻole', 'Over the Rainbow', '2010-04-12', 0, 'img/OverTheRainbow.jpg', 'music/someWhereOverTheRainbow.mp3', 4),
-(36, 'Is This Love', 'Bob Marley & The Wailers', 'Kaya', '1978-07-07', 0, 'img/BobMarley.jpg', 'music/isThisLove.mp3', 4),
-(37, 'Hoja en Blanco', 'Dread Mar I', '10 Años ', '2016-11-04', 0, 'img/DreadMarI.jpg', 'music/hojaEnBlanco.mp3', 4),
-(38, 'Tiempo', 'Rabeat & Underdann', 'Tiempo', '2016-10-13', 0, 'img/Rabeat.jpg', 'music/', 4),
-(39, 'Un Nuevo Día', 'Zona Ganjah', 'Poder', '2010-06-10', 0, 'img/ZonaGanjah.jpg', 'music/', 4),
-(40, 'Lose Yourself', 'Eminem', 'Lose Yourself', '2002-06-22', 0, 'img/eminem.jpg', 'music/loseYourself.mp3', 6),
-(41, 'Rap God', 'Eminem', 'Sencillo', '2013-10-13', 0, 'img/eminem.jpg', '1DfSPlh7assLFojwydiOnl7vKQRIGM3rZ', 6),
-(42, 'In Da Club', '50 Cent', 'In Da Club', '2009-06-09', 0, 'img/50Cent.jpg', 'music/inDaClub.mp3', 6),
-(43, 'Candy Shop', '50 Cent', 'Candy Shop', '2009-06-16', 0, 'img/50Cent.jpg', 'music/candyShop.mp3', 6),
-(44, 'goosebumps', 'Travis Scott', 'Birds in the Trap Sing McKnight', '2017-04-14', 0, 'img/TravisScott.jpg', 'music/goosebumps.mp3', 6),
-(45, 'Black And Yellow', 'Wiz Khalifa', 'Black and Yellow (feat. Juicy J, Snoop Dogg & T-Pain)', '2011-01-07', 0, 'img/wizKhalifa.jpg', 'music/blackAndYellow.mp3', 6),
-(46, 'En Boca De Tantos', 'Porta', 'En Boca De Tantos', '2009-06-26', 0, 'img/porta.jpg', 'music/enBocaDeTantos.mp3', 6),
-(50, 'La Marca de La Gorra', 'Mala Fama', 'La Tonga', '2017-12-19', 0, 'img/619dc90539979.jpg', 'music/LaMarcadeLaGorra.mp3', 1),
-(51, 'La Mas Linda Del Salon', 'Los Nota Lokos', 'Los Nota Lokos', '2012-09-30', 0, 'img/losNotaLokos.jpg', 'music/laMasLindaDelSalon.mp3', 1),
-(52, 'Re Loco ', 'De La Calle', 'Mas Negro Que La Noche', '2016-07-26', NULL, 'img/deLaCalle.jpg', '1H367UvZai_1knot0PsP9tr-XQhJD3XZy', 1),
-(53, 'De Regreso al Penal ', 'Pala Ancha', 'Cumbia Callejera', '2001-07-19', NULL, 'img/PalaAncha.jpg', 'music/deRegresoAlPenal.mp3', 1),
-(54, 'solo quiero amarte ', 'La Champion Liga', ' Confía en Mí', '2010-07-24', NULL, 'img/LaChampionLiga.jpg', 'music/soloQuieroAmarte.mp3', 1),
-(55, 'Mujer Yo te amo', 'Mc Caco', 'Mc Caco', '2010-06-25', NULL, 'img/mcCaco.jpg', 'music/yoTeAmo.mp3', 1),
-(56, 'Hoy Volví a Verte ', 'El Retutu', 'El Retutu', '2011-01-28', NULL, 'img/ElRetutu.jpg', 'music/', 1),
-(57, 'Una Wacha Piola', 'De La Calle', 'De La Calle', '2016-07-26', NULL, 'img/deLaCalle.jpg', 'music/', 1),
-(58, 'Llama', 'Marka Akme', 'Marka Akme', '2016-05-16', NULL, 'img/markaAkme.jpg', 'music/', 1),
-(59, 'Wup Wup!', 'RPM', 'Revolucion Por Minuto RPM  ', '2014-08-05', NULL, 'img/619dac264e1e7.jpg', 'music/', 1);
+(11, 'Fresco', 'WOS', 'Carabana', '2019-10-04', 0, 'img/619ee5fe663ef.jpg', '1Thr23Xgmlref9njs4BtyVa1G1uzVnD0_', 2),
+(12, 'Llenos de magia', 'La Vela Puerca', 'A contra luz', '2016-07-08', 0, 'img/619ee5968bc1f.jpg', '1SCW1V9w0CmXPRVImJrGwIUHnJVOVJCtp', 3),
+(13, 'Viejo Karma!', 'Las Pastillas Del Abuelo', 'Desafios', '2017-11-12', 0, 'img/619ee5a216eca.jpg', '1H5_IzcYETPFAtr_v8TZF5ORBLFTuvM-Y', 3),
+(14, 'Demaciado Loco', 'Paulo Londra', 'Home Run', '2019-05-23', 1, 'img/619ee546a2d5c.jpg', '12PkyT6eF-zoZmTNKCOXbp0APqg2B4HE1', 2),
+(15, 'Loco', 'Tiago PZK', 'Loco', '2021-09-16', 0, 'img/619ee56ba2d08.jpg', '1gW_pPSIjuICLcz0wkuwo03JHg2Z7srMR', 2),
+(16, 'Hazmelo', 'Tiago PZK', 'Hazmelo', '2021-05-26', 1, 'img/619ee58123050.jpg', '1ZUtWLhW4cBCNgj8-JvL99PJNWdvx1FBy', 2),
+(17, 'Como si no importara', 'Emilia & Duki', 'Como si no importara', '2021-07-13', 0, 'img/ComoSiNoImportara.jpg', '10HmSdl5O4G_yjcX81WfARhrYjYoXRAZh', 2),
+(18, 'ADEMAS DE MI REMIX', 'Rusherking, Tiago PZK, KHEA, LIT Killah, Duki, Maria Becerra', 'A demas de mi', '2021-03-04', 1, 'img/AdemasDeMiRemix.jpg', '1X5cR-hZV1ui5HNvBHo40wrMIYhHOEw1k', 2),
+(19, 'Nat Geo', 'Falke 912, Bhavi Ft. LIT Killah', 'Nat Geo', '2021-07-15', 0, 'img/NatGeo.jpg', '1EVYEGNGIKo78wxQRvH6szNx7oCoHSfxz', 2),
+(20, 'No me conocen (REMIX)', 'BANDIDO, DUKI, REI, TIAGO PZK', 'No me conocen', '2021-06-16', 1, 'img/NoMeConocen (REMIX).jpg', '1lZhW85bHXHDQlgX8lN6tBpyN0XGKTcif', 2),
+(21, 'Prende la Cámara', 'FMK, Tiago PZK', 'Prende la Cámara', '2021-07-01', 0, 'img/PrendeLaCámara.jpg', '1WE40S98GgpFS2R154UpGfP_2N6H2Aidh', 2),
+(22, 'Rápido Lento', 'Emilia, Tiago PZK', 'Rápido Lento', '2021-09-30', 0, 'img/RápidoLento.jpg', '16F0nWdnANnTyDwUmCw_5yXgFNRqCkyOB', 2),
+(26, 'M1 A1', 'Gorillaz', 'M1 A1', '2015-02-12', 0, 'img/gorillaz.jpg', '1L6H21S_IZGwOBreKz8jB_4F8JWFjDhwF', 3),
+(27, 'Ruta 66', 'Pappo\'s Blues', 'Caso Cerrado', '1995-03-15', 0, 'img/papo.jpg', '12D0A_rulcyAW5CPP4-pDBhS0drYjs7qO', 3),
+(28, 'Balada Del Diablo y La Muerte', 'La Renga', 'Despedazado por mil partes', '1996-07-21', 0, 'img/LaRenga.jpg', '15p5ym2et0K4imMe7scR_7hxbDzvFgDnn', 3),
+(29, 'Toxicity ', 'System Of A Down', 'System Of A Down', '2001-09-04', 0, 'img/SystemOfaDown.jpg', '1pHcIy09ECzUHp2u0JfNu-QCWqQYif3VL', 3),
+(30, 'Crimen', 'Gustavo Cerati', 'Ahí Vamos', '2006-10-05', 0, 'img/cerati.jpg', '1IR7QulRHp5oHGa3SVfpP0ST9do1GKMGM', 3),
+(31, 'JIJIJI', 'Los Redondos', 'Oktubre', '1986-10-18', 1, 'img/indioSolari.jpg', '1tt6rJxJ70zRN4dyGVexYOw29t8aAFtId', 3),
+(32, 'GALANG', 'Alika', 'EDUCATE YOURSELF', '2008-04-19', 0, 'img/619ee5b419da4.jpg', '12Tw-CKGDuXqnUKEsEsD1KuRchSztc5CK', 4),
+(33, 'Te robaste mi corazon', 'Fidel Nadal', 'Forever together', '2010-10-10', 0, 'img/619ee5c17947e.jpg', '1gtEP7Xu7xIbZi2PUdswPgqDBa0gN-_Py', 4),
+(34, 'International love ', 'Fidel Nadal', 'INTERNATIONAL LOVE', '2008-10-16', 0, 'img/619ee5cf1d773.jpg', '1MF106bi8_AM3ctsfFgZqCwJLirRniX60', 4),
+(35, 'Somewhere over the Rainbow', 'Israel ', 'Over the Rainbow', '2010-04-12', 0, 'img/OverTheRainbow.jpg', '1DYaTkQI4euqJg775TJ9u0rbY0wpA6gHc', 4),
+(36, 'Is This Love', 'Bob Marley & The Wailers', 'Kaya', '1978-07-07', 0, 'img/BobMarley.jpg', '12DGLMDSaCE8RFS1I2h6WyCda9Ux7rMtV', 4),
+(37, 'Hoja en Blanco', 'Dread Mar I', '10 Años ', '2016-11-04', 0, 'img/DreadMarI.jpg', '1Vj3aJYNCVH-oE_dUD1eq7RTy2oRJybkf', 4),
+(38, 'Tiempo', 'Rabeat & Underdann', 'Tiempo', '2016-10-13', 0, 'img/Rabeat.jpg', '1zQYwqXdasrObT1zHyawWmCAgLwRxgd_-', 4),
+(39, 'Un Nuevo Día', 'Zona Ganjah', 'Poder', '2010-06-10', 0, 'img/ZonaGanjah.jpg', '1rE5070hL71yIAZxYbp729iT5O7zpZ4jq', 4),
+(40, 'Lose Yourself', 'Eminem', 'Lose Yourself', '2002-06-22', 0, 'img/619ee6a3df274.jpg', '1BmI4jA6Gi-UmWukWU1W5jAD9dHUkSGR-', 6),
+(41, 'Rap God', 'Eminem', 'Sencillo', '2013-10-13', 0, 'img/619ee6b493c68.jpg', '1DfSPlh7assLFojwydiOnl7vKQRIGM3rZ', 6),
+(42, 'In Da Club', '50 Cent', 'In Da Club', '2009-06-09', 0, 'img/619ee7833feeb.jpg', '1YZOYw_y2f269jNrrcI9jjhSHkiyxz2Lc', 6),
+(43, 'Candy Shop', '50 Cent', 'Candy Shop', '2009-06-16', 0, 'img/50Cent.jpg', '1UhaxUvz2HNMr_rUyN3nu2HZeDnaoqvqW', 6),
+(44, 'goosebumps', 'Travis Scott', 'Birds in the Trap Sing McKnight', '2017-04-14', 0, 'img/TravisScott.jpg', '1YQzvh6au1PRLFv_vWFqYex5jCwnbq8o6', 6),
+(45, 'Black And Yellow', 'Wiz Khalifa', 'Black and Yellow (feat. Juicy J, Snoop Dogg & T-Pain)', '2011-01-07', 0, 'img/wizKhalifa.jpg', '1PcVBSzSHlqQb6VxQh09wCMFYnAmMQxwQ', 6),
+(46, 'En Boca De Tantos', 'Porta', 'En Boca De Tantos', '2009-06-26', 0, 'img/porta.jpg', '1wX5Uibz0bDTM2xF6qzbsc2JCdm6NUhAk', 6),
+(50, 'La Marca de La Gorra', 'Mala Fama', 'La Tonga', '2017-12-19', 0, 'img/619ee4cc2dac1.jpg', '1O2eZe--NGkYFvVbo2ohbx4HaLT_xStWV', 1),
+(51, 'La Mas Linda Del Salon', 'Los Nota Lokos', 'Los Nota Lokos', '2012-09-30', 0, 'img/619ee5005cc7e.jpg', '1DZ2lQ5_3YBZQ1eYSBloDRSMIlPO1Rh2i', 1),
+(52, 'Re Loco ', 'De La Calle', 'Mas Negro Que La Noche', '2016-07-26', NULL, 'img/619ee5333969b.jpg', '1H367UvZai_1knot0PsP9tr-XQhJD3XZy', 1),
+(53, 'De Regreso al Penal ', 'Pala Ancha', 'Cumbia Callejera', '2001-07-19', NULL, 'img/619ee9b48f161.jpg', '17rIAPAIkwkHBNQgbQ_zfrb0cV3pp4kvb', 1),
+(54, 'solo quiero amarte ', 'La Champion Liga', ' Confía en Mí', '2010-07-24', NULL, 'img/619ee9dbb01ef.jpg', '14oLQrCUYeAYoFZnzoHRYBfs5_MuItMMn', 1),
+(55, 'Mujer Yo te amo', 'Mc Caco', 'Mc Caco', '2010-06-25', NULL, NULL, '1BtHXYyBXyrQN9fPxUWF8XuGgsbnmeWCv', 1),
+(56, 'Hoy Volví a Verte ', 'El Retutu', 'El Retutu', '2011-01-28', NULL, 'img/ElRetutu.jpg', '1wUi8pMAEKvr9HGHtVC8xa4I684DU-r2z', 1),
+(57, 'Una Wacha Piola', 'De La Calle', 'De La Calle', '2016-07-26', NULL, 'img/deLaCalle.jpg', '1g3C5pcaa6bXpM5OfZfqbaLBzlHHq9tpx', 1),
+(58, 'Llama', 'Marka Akme', 'Marka Akme', '2016-05-16', NULL, 'img/markaAkme.jpg', '1ufha_etnkkOgaRVGk3jTFLx1Gwgy3QGk', 1),
+(59, 'Wup Wup!', 'RPM', 'Revolucion Por Minuto RPM  ', '2014-08-05', NULL, 'img/619dac264e1e7.jpg', '19ZhpZzmD2n8yAyNE4kkvLq5pi79i7IIM', 1);
 
 -- --------------------------------------------------------
 
@@ -199,7 +199,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id_comentarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id_comentarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT de la tabla `generos`
@@ -211,7 +211,7 @@ ALTER TABLE `generos`
 -- AUTO_INCREMENT de la tabla `musica`
 --
 ALTER TABLE `musica`
-  MODIFY `id_musica` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `id_musica` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
